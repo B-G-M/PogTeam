92b2627905a2c4fbc0c82b87c1f3b4266c7259ef
B-G-M <bgm99@mail.ru>
2022-11-21 19:42:27 +0300

    server_upd

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System.Net.Sockets;
 using System.Net;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Server
 {
@@ -10,153 +12,188 @@ namespace Server
 		{
 			IPEndPoint ipPoint = new IPEndPoint(IPAddress.Any, 1457);
 			Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-			public void Start() 
+			List<Game> games = new List<Game>();
+			public void Start()
 			{
+
 				Console.WriteLine("Сервер запущен");
 				socket.Bind(ipPoint);
-				socket.Listen(2);
-				
-				Game game = new(socket.Accept(), socket.Accept());
-				game.Start();
+				socket.Listen();
+
+				while(true)
+				{
+					Game game = new Game();
+					game.NewPlayer(socket.Accept());
+					game.NewPlayer(socket.Accept());
+					while (game.GameRdy()) { };
+
+					new Thread(() => game.Start()).Start();
+					games.Add(game);
+				}
 			}
 		}
 
 		class Game
 		{
-			public Game(Socket p1,Socket p2) 
+			public Game()
 			{
 				Random rnd = new Random();
-				player1 = new Player(p1, rnd.Next(0,99));
-				player2 = new Player(p2, rnd.Next(0,99));
-
-				
-				if (rnd.Next(0, 1) == 0) 
+				if (rnd.Next(0, 1) == 0)
 				{
-					player1.side = 0;
-					player2.side = 1;
+					player1Side = 0;
+					player2Side = 1;
 				}
 				else
 				{
-					player1.side = 1;
-					player2.side = 0;
+					player1Side = 1;
+					player2Side = 0;
 				}
-				
-            }
-			public Player? player1;
-			public Player? player2;
+			}
 
-			
+			public Player player1;
+			public Player player2;
+			int player1Side = 0;
+			int player2Side = 1;
+			bool player1Connect = false;
+			bool player2Connect = false;
+
 			public void Start()
 			{
 				string? msg = "";
-				while(msg != "stop")
-				{
-					player1.ReciveMsg();
-					player2.ReciveMsg();
+				Thread recP1 = new Thread(player1.ReciveMsg);
+				Thread recP2 = new Thread(player2.ReciveMsg);
+				recP1.Start();
+				recP2.Start();
 
-					//msg = Console.ReadLine();
-					//var msgS = msg.Split(" ");
-					//if (msgS[0] == "p1")
-					//	player1.SendMsg(msg);
-					//else if (msgS[0] == "p2")
-					//	player2.SendMsg(msg);
-					//else if (msgS[0] == "pp")
-					//{
-					//	player1.SendMsg(msg);
-					//	player2.SendMsg(msg);
-					//}
-					//if (msg == "stop")
-					//{
-					//	player1.socket.Close();
-					//	player2.socket.Close();
-					//}
-					//msg = "";
+				while (true)
+				{
+					
 				}
-				
 			}
-			
+			public bool GameRdy()
+			{
+				if (player1Connect && player2Connect)
+					return true;
+				else return false;
+			}
+
+			public async void NewPlayer(Socket socket)
+			{
+				await Task.Run(() =>
+				{
+					Player player = new Player(socket);
+					player.ReciveMsg();		
+
+					if (player1Connect == false)
+					{
+						player1Connect = true;
+						player1 = player;
+						player1.side = player1Side;
+					}
+					else
+					{
+						player2Connect = true;
+						player2 = player;
+						player2.side = player2Side;
+					}
+						
+				});
+			}
 		}
 
 
 		class Player
 		{
-			public Player(Socket socket,int id)
+			public Player(Socket socket)
 			{
+				Random rnd = new Random();
+
 				this.socket = socket;
-				this.id = id;
-				Console.WriteLine("Игрок {0} подключился",id);
+				this.id = rnd.Next(0, 99);//поменять генерацию 
+				Console.WriteLine("Игрок {0} подключился", id);
 			}
-			public string nickName = "";
-			public string password = "";
+
+			public string nickName;
+			public string password;
 			public int id;
 			public float stickY;
 			public int side;
 
 			public bool IsReady = false;
 
-			public Socket? socket;
+			public Socket socket;
 
-			public bool SendMsg(string msg) 
+			public bool SendMsg(string msg)
 			{
 				byte[] ansArr = Encoding.UTF8.GetBytes(msg);
 				socket.Send(ansArr);
 				return true;//прикрути проверку на отправку
 			}
-			public string ReciveMsg()
-			{
-				byte[] msgArr = new byte[1024];
-				socket.Receive(msgArr);
-				string msg = Encoding.UTF8.GetString(msgArr);
-				Console.WriteLine("Получено от игрока {0}: {1}", id, msg);
 
-				CommandProcessing(msg);
-				return msg;
+			public void ReciveMsg()
+			{
+				while(true)
+				{
+					byte[] msgArr = new byte[1024];
+					socket.Receive(msgArr);
+					string msg = Encoding.UTF8.GetString(msgArr);
+					Console.WriteLine("Получено от игрока {0}: {1}", id, msg);
+
+					foreach (var req in msg.Split(";"))
+					{
+						CommandProcessing(req);
+					}
+				}
+				
 			}
-			private int AuthPlayer(string login, string pass)
-            {
-				//if (nickName == login && pass == password)
-				//{
-				//return true;
-				//}
-				return id;
-            }
+
+			private bool AuthPlayer(string login, string password)
+			{
+				this.nickName = login;
+				this.password = password;
+				return true;
+			}
+
 			private string CheckSide()
-            {
+			{
 				return side.ToString();
-            }
-			private bool ChangeReady(string Readyness)
-            {
-				if (Readyness == "true" && !IsReady) IsReady = true;
-				else if (Readyness == "false" && IsReady) IsReady = false;
+			}
+
+			private bool ChangeReady(string readyness)
+			{
+				if (readyness == "true" && !IsReady) IsReady = true;
+				else if (readyness == "false" && IsReady) IsReady = false;
 				return IsReady;
-            }
-            private void CommandProcessing(string Request)
-            {
-                string[] requestPart = Request.Split(" ");
-                switch (requestPart[0])
-                {
-                    case "auth":
-						//if (AuthPlayer(requestPart[1], requestPart[2])) SendMsg("OK");
-						//else SendMsg("ERROR");
-						SendMsg("id " + AuthPlayer(requestPart[1], requestPart[2]).ToString());
-						Console.WriteLine("Отправлено игроку {0}: {1}",id,"id " + AuthPlayer(requestPart[1], requestPart[2]));
-                        break;
-                    case "ch_side":
+			}
+			private void CommandProcessing(string request)
+			{
+				string[] requestPart = request.Split("_");
+				switch (requestPart[0])
+				{
+					case "auth":
+						if (AuthPlayer(requestPart[1], requestPart[2]))
+						{
+							SendMsg("OK");
+							Console.WriteLine("Отправлено игроку {0}: {1}", id, "id " + AuthPlayer(requestPart[1], requestPart[2]));
+						}
+						else SendMsg("ERROR");
+
+						break;
+					case "ch_side":
 						SendMsg(CheckSide());
-                        break;
-                    case "rdy":
+						break;
+					case "rdy":
 						if (ChangeReady(requestPart[2])) SendMsg("Ready");
 						else SendMsg("Not Ready");
-                        break;
-                    case "   ":
-                        Console.WriteLine();
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
+						break;
+					case "   ":
+						Console.WriteLine();
+						break;
+					default:
+						break;
+				}
+			}
+		}
 
 		static void Main(string[] args)
 		{
