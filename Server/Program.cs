500c334c50b8a1c32e2e8dea35a50131b2d4a57c
Nikolay Vrzesinsky <15nk33l49@gmail.com>
2022-11-26 12:45:48 +0300

    -

--- Server/Program.cs
+++ Server/Program.cs
@@ -309,9 +309,9 @@ namespace Server
 		{
 			using (ApplicationContext db = new ApplicationContext())
 			{
-
-				/*User user1 = new User { Email = "Tom@mail.com", Password = "33",WinGames= 0 };
-				User user2 = new User { Email = "Jaff@mail.com", Password = "44", WinGames = 5 };
+				/*
+				User user1 = new User { Email = "Kirill@mail.com", Password = "55",WinGames= 0 };
+				User user2 = new User { Email = "Kolya@mail.com", Password = "66", WinGames = 5 };
 
 				db.Users.AddRange(user1, user2);
 				db.SaveChanges();
